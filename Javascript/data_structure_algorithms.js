44b51f36d8c3bc345816de5f359f1dbf0cfa819e
Candice Han <jeeyoung.han82@gmail.com>
2020-09-29 11:39:03 +0900

    ADD data_structure_algorithms.js

---
+++ Javascript/data_structure_algorithms.js
@@ -0,0 +1,28 @@
+// Stacks
+
+// functions: push, pop, peek, length
+
+// console.log(letters);
+// ['r', 'a', 'c','e', 'c', 'a','r']
+
+const isPalindrome = (str) => {
+  const letters = [];
+
+  let rword = "";
+
+  for (i = 0; i < str.length; i++) {
+    letters.push(str[i]);
+  }
+
+  for (i = 0; i < str.length; i++) {
+    rword += letters.pop();
+  }
+
+  if (rword === str) {
+    console.log(str + " is a palindrome.");
+  } else {
+    console.log(str + " is not a palindrome.");
+  }
+};
+
+isPalindrome("racecar");
