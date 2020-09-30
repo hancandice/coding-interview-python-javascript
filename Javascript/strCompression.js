0ce48698c417772482e3cbfa9eb9dcee6a7171bd
Candice Han <jeeyoung.han82@gmail.com>
2020-09-30 18:46:01 +0900

    ADD stringCompression.js

---
+++ Javascript/strCompression.js
@@ -0,0 +1,25 @@
+// implement a method to perform a basic string compression using the counts of repeated characters.
+
+const stringCompression = (str) => {
+  const n = str.length;
+  let count = 1;
+  let output = "";
+  let uniqCount = 0;
+  for (let i = 0; i < n; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      output += str[i] + count;
+      count = 1;
+      uniqCount++;
+    }
+  }
+  if (uniqCount === n) return str;
+  else return output;
+};
+
+const word = "Jeyoung";
+
+console.log({
+  word: stringCompression(word),
+});
