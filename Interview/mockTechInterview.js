d3b74a7be57ffaf7b5981949e3d347fc644ae7f8
Candice Han <jeeyoung.han82@gmail.com>
2020-10-01 22:48:51 +0900

    ADD Interview folder

---
+++ Interview/mockTechInterview.js
@@ -0,0 +1,23 @@
+/* 
+Quick Introduction
+
+1. fitness platform. web/mobile app. dynamic map. React/node.js 
+2. Introducing myself 
+    - projects / my role 
+    - web apps / web development 
+    - web / front-end engineer 
+    // team project or working alone 
+3. my main role 
+    - Node.js part & angular part 
+4. Javascript questions 
+    # very generic web-based question 
+        - https negotitation step, detail handcheck 
+        - CORS 
+        - this keyword 
+        - closure 
+        - how prototype in javascript works?
+        
+
+
+
+*/
